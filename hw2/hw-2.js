0aa70567511f14fc87d1d703cd4b4e00c81440f2
alexandersergeevi <alexandersergeevi@yandex.ru>
2023-04-25 21:32:46 +0300

    Работа с JS

---
+++ hw2/hw-2.js
@@ -0,0 +1,75 @@
+// Задание 1
+
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+
+// Задание 2
+
+let year = 2007;
+alert (year);
+
+// Задание 3
+
+let creator = `Брендан Эйх`;
+alert (creator);
+
+// Задание 4
+
+let b = 10;
+let c = 2;
+alert (`Результат сложения ${b+c}`);
+alert (`Результат вычитания ${b-c}`);
+alert (`Результат произведения ${b*c}`);
+alert (`Результат деления ${b/c}`);
+
+// Задание 5
+
+let result = 2**5;
+alert (result);
+
+// Задание 6
+
+let aa = 9;
+let bb = 2;
+alert (aa%bb);
+
+// Задание 7
+
+let num = 1;
+num+=5;
+num-=3;
+num*=7
+num/=3
+num++;
+num--;
+alert (num);
+
+// Задание 8
+
+let age = prompt ("Сколько вам лет?");
+alert (age);
+
+// Задание 9
+
+const user = {
+	name: 'Олег',
+	age: 50,
+    isAdmin: true
+}
+
+user.cityOfResidence = 'Москва';
+
+user.age = 55;
+
+delete user.cityOfResidence;
+
+let info = prompt("Какую информацию хотите узнать о пользователе?", "name", "age", "isAdmin");
+console.log(user[info]);
+alert (user[info]);
+
+// Задание 10
+
+let userName = prompt ("Как Вас зовут?");
+alert (`Привет, ${userName}!`);
